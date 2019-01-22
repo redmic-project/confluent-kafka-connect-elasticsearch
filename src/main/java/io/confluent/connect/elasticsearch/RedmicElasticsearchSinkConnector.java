96a53861b31598b2e6d013353fd239b0e10329b7
Noel Alonso <noel@redmic.es>
2019-01-22 15:33:16 +0000

    Renombra conector para generarizarlo
    De esta forma, este conector servirá para cualquier tipo de dato que
    presente las necesidades que aporta esta implementación

--- src/main/java/io/confluent/connect/elasticsearch/RedmicElasticsearchSinkConnector.java
+++ src/main/java/io/confluent/connect/elasticsearch/RedmicElasticsearchSinkConnector.java
@@ -27,7 +27,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class GeoJsonElasticsearchSinkConnector extends SinkConnector {
+public class RedmicElasticsearchSinkConnector extends SinkConnector {
 
   private Map<String, String> configProperties;
 
